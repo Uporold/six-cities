01bacfae55e98b21bc374dbc2abe15c2613863fb
Uporold <crubick1337@gmail.com>
2020-08-19 21:24:54 +0300

    Отрисовка объявлений из props

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Main from "../main/main";
 
-function App() {
-  return <Main />;
+function App({ adTitles }) {
+  return <Main adTitles={adTitles} />;
 }
+
+App.propTypes = {
+  adTitles: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+};
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app";
 
+const fakeAdTitles = [
+  `Beautiful & luxurious apartment at great location`,
+  `Wood and stone place`,
+  `Canal View Prinsengracht`,
+  `Nice, cozy, warm big bed apartment`,
+];
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App adTitles={fakeAdTitles} />,
   document.getElementById("root")
 );
