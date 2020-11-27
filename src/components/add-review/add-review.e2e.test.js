0649119528ebb22cc82b871bbc1912b30b8f1367
Uporold <crubick1337@gmail.com>
2020-11-27 18:09:18 +0300

    Добавил enzyme-тест компонента PlaceCard.

--- src/components/add-review/add-review.e2e.test.js
+++ src/components/add-review/add-review.e2e.test.js
@@ -5,33 +5,51 @@ import { AddReview } from "./add-review";
 
 configure({ adapter: new Adapter() });
 
-it(`Should local state change`, () => {
-  const handleFormSubmit = jest.fn();
-  const handleTextInputFocus = jest.fn();
-  const wrapper = shallow(
-    <AddReview
-      hotelId={1}
-      isReviewSending={false}
-      isSendingError={false}
-      onFormSubmit={handleFormSubmit}
-      onTextInputFocus={handleTextInputFocus}
-    />
-  );
-  wrapper
-    .find(".reviews__rating-form")
-    .simulate(`change`, { target: { value: 3 } });
-  expect(wrapper.state().stars).toEqual(3);
-
-  wrapper
-    .find(".reviews__textarea")
-    .simulate(`change`, { target: { value: `Test text` } });
-  expect(wrapper.state().comment).toEqual(`Test text`);
-
-  wrapper.find(`form.reviews__form`).simulate(`submit`, {
-    preventDefault: jest.fn(),
-    target: { reset: jest.fn() },
+describe(`AddReview tests`, () => {
+  let handleFormSubmit;
+  let handleTextInputFocus;
+  let wrapper;
+  beforeEach(() => {
+    handleFormSubmit = jest.fn();
+    handleTextInputFocus = jest.fn();
+    wrapper = shallow(
+      <AddReview
+        hotelId={1}
+        isReviewSending={false}
+        isSendingError={false}
+        onFormSubmit={handleFormSubmit}
+        onTextInputFocus={handleTextInputFocus}
+      />
+    );
   });
-  expect(handleFormSubmit).toHaveBeenCalledTimes(1);
-  expect(wrapper.state().stars).toEqual(0);
-  expect(wrapper.state().comment).toEqual(``);
+
+  it(`Should rating state change`, () => {
+    wrapper
+      .find(".reviews__rating-form")
+      .simulate(`change`, { target: { value: 3 } });
+    expect(wrapper.state().stars).toEqual(3);
+  });
+
+  it(`Should comment state change`, () => {
+    wrapper
+      .find(".reviews__textarea")
+      .simulate(`change`, { target: { value: `Test text` } });
+    expect(wrapper.state().comment).toEqual(`Test text`);
+  });
+
+  it(`Should textarea focus call function`, () => {
+    wrapper.find(".reviews__textarea").simulate(`focus`);
+    expect(handleTextInputFocus).toHaveBeenCalledTimes(1);
+  });
+
+  it(`Should reset state after submit button click`, () => {
+    wrapper.setState({ comment: `Test comment`, stars: 5 });
+    wrapper.find(`form.reviews__form`).simulate(`submit`, {
+      preventDefault: jest.fn(),
+      target: { reset: jest.fn() },
+    });
+    expect(handleFormSubmit).toHaveBeenCalledTimes(1);
+    expect(wrapper.state().stars).toEqual(0);
+    expect(wrapper.state().comment).toEqual(``);
+  });
 });
--- src/components/place-card/place-card.e2e.test.js
+++ src/components/place-card/place-card.e2e.test.js
@@ -1,28 +1,53 @@
 import React from "react";
-import Enzyme, { shallow, mount } from "enzyme";
+import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import PlaceCard from "./place-card";
+import { PlaceCard } from "./place-card";
 import { hotels } from "../../mock/offers";
 
 Enzyme.configure({
   adapter: new Adapter(),
 });
 
-it(`Should card be hovered`, () => {
-  const onCardHover = jest.fn();
+describe(`Should card be hovered`, () => {
+  let onCardHover;
+  let onFavButtonClick;
+  let smallMovieCard;
 
-  const smallMovieCard = shallow(
-    <PlaceCard
-      onPlaceCardClick={() => {}}
-      hotel={hotels[0]}
-      onHover={onCardHover}
-    />
-  );
+  beforeEach(() => {
+    onCardHover = jest.fn();
+    onFavButtonClick = jest.fn();
 
-  const card = smallMovieCard.find(`.cities__place-card`);
+    smallMovieCard = shallow(
+      <PlaceCard
+        hotel={hotels[0]}
+        clearErrorHotelIds={jest.fn()}
+        errorHotelIds={[]}
+        onButtonClick={onFavButtonClick}
+        onCardHover={onCardHover}
+        pageType="MAIN"
+      />
+    );
+  });
 
-  //card.props().onMouseOver();
-  card.simulate(`mouseenter`, hotels[0]);
-  expect(onCardHover.mock.calls.length).toBe(1);
-  expect(onCardHover.mock.calls[0][0]).toMatchObject(hotels[0]);
+  it(`Card mouse enter and mouseout tests`, () => {
+    const card = smallMovieCard.find(`.cities__place-card`);
+
+    card.props().onMouseEnter();
+    card.props().onMouseOut();
+
+    expect(onCardHover.mock.calls.length).toBe(2);
+    expect(onCardHover.mock.calls[0][0]).toBe(hotels[0].id);
+    expect(onCardHover.mock.calls[1][0]).toBe(-1);
+  });
+
+  it(`Card favorite button test`, () => {
+    const button = smallMovieCard.find(`button.place-card__bookmark-button`);
+    const evt = { preventDefault: jest.fn() };
+
+    button.props().onClick(evt);
+
+    expect(onFavButtonClick.mock.calls.length).toBe(1);
+    expect(onFavButtonClick.mock.calls[0][0]).toBe(hotels[0].id);
+    expect(onFavButtonClick.mock.calls[0][1]).toBe(!hotels[0].isFavorite);
+  });
 });
