7a6e97e092d30c682696dcfe38dddc3c22ca85e0
Uporold <crubick1337@gmail.com>
2020-12-14 21:14:54 +0300

    Компонент Main на хуках.

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
 import Header from "../../components/header/header";
-import { projectPropTypes } from "../../utilites/project-prop-types";
 import CitiesTabs from "../../components/cities-tabs/cities-tabs";
 import MainEmpty from "../../components/main-empty/main-empty";
 import PlacesContainer from "../../components/places-container/places-container";
-import { getHotelsSortedByCity } from "../../redux/data/selectors";
+import { useHotelsSortedByCity } from "../../redux/data/hooks/selectors";
 
-const Main = ({ hotels }) => {
+const Main = () => {
+  const hotels = useHotelsSortedByCity();
   return (
     <div
       className={`page page--gray page--main ${
@@ -27,13 +25,4 @@ const Main = ({ hotels }) => {
   );
 };
 
-Main.propTypes = {
-  hotels: PropTypes.arrayOf(projectPropTypes.HOTEL.isRequired).isRequired,
-};
-
-const mapStateToProps = (state) => ({
-  hotels: getHotelsSortedByCity(state),
-});
-
-export { Main };
-export default connect(mapStateToProps, null)(Main);
+export default Main;
